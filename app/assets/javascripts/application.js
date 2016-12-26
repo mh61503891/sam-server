fbcdbb3696a4f71ed7f84dada1a0fc2394a88940
Masayuki Higashino <msyk@hgsn.info>
2016-12-26 16:54:42 +0900

    add bootstrap 4.0.0-alpha.5

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require tether
+//= require bootstrap/dist/js/bootstrap
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
