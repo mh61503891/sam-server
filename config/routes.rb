2d46b04402e3ae76db20af5f0165c2bcc1125bf3
Masayuki Higashino <msyk@hgsn.info>
2016-12-27 12:25:38 +0900

    bundle exec rails g scaffold license

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :licenses
   resources :applications
   resources :computers
   devise_for :users
---
+++ db/migrate/20161227032531_create_licenses.rb
@@ -0,0 +1,8 @@
+class CreateLicenses < ActiveRecord::Migration[5.0]
+  def change
+    create_table :licenses do |t|
+
+      t.timestamps
+    end
+  end
+end
