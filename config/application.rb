c0a1cefefa6ebd54454f434eccee45fb8dfcc2aa
Masayuki Higashino <msyk@hgsn.info>
2016-12-31 18:47:47 +0900

    add time_zone and i18n default locale

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,7 @@ module SunabaServer
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.paths << Rails.root.join('node_modules')
+    # config.time_zone = 'Tokyo'
+    # config.i18n.default_locale = :ja
   end
 end
