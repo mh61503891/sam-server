bf1ae5c0395c3c31d9429016aa26fcbe624b0fe9
Masayuki Higashino <msyk@hgsn.info>
2017-01-01 16:20:15 +0900

    change database schema of users

--- db/migrate/20161223083408_devise_create_users.rb
+++ db/migrate/20161223083408_devise_create_users.rb
@@ -1,23 +1,35 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.0]
+
   def change
+
     create_table :users do |t|
-      # Database authenticatable
+      # Authenticatable
       t.string :uid, null:false
-      t.string :email, null:false, default:''
-      t.string :encrypted_password, null:false, default:''
+      t.string :encrypted_password
       # Rememberable
       t.datetime :remember_created_at
       # Trackable
-      t.integer  :sign_in_count, default:0, null:false
+      t.integer :sign_in_count, default:0, null:false
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
       t.string :current_sign_in_ip
       t.string :last_sign_in_ip
       # Timestamps
-      t.timestamps null: false
+      t.timestamps null:false
+      # Options
+      t.string :email
+      t.string :name
+      t.string :name_ruby
+      t.string :name_en
+      t.string :title
+      t.string :title_ruby
+      t.string :title_en
     end
+
     add_index :users, :uid, unique:true
-    add_index :users, :email, unique:true
-    add_index :users, :reset_password_token, unique:true
+    %i(email name name_ruby name_en title title_ruby title_en).each do |column|
+      add_index :users, column
+    end
+
   end
 end
