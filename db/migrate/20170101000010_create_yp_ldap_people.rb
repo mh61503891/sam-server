e7744b31d63be3ac228fcb51df1d5285b84d2e63
Masayuki Higashino <msyk@hgsn.info>
2017-01-20 19:22:07 +0900

    add migration: yp_ldap_people

---
+++ db/migrate/20170101000010_create_yp_ldap_people.rb
@@ -0,0 +1,20 @@
+class CreateYpLdapPeople < ActiveRecord::Migration[5.0]
+  def change
+    create_table :yp_ldap_people do |t|
+      # person
+      t.string :dn, null:false
+      t.string :sn              # [must]
+      t.string :cn              # [must]
+      t.string :telephoneNumber # [may]
+      t.string :description     # [may]
+      # organizationalPerson
+      t.string :title           # [may]
+      # inetOrgPerson
+      t.string :displayName     # [may]
+      t.string :givenName       # [may]
+      t.string :mail            # [may]
+      t.timestamps
+    end
+    add_index :yp_ldap_people, :dn, unique:true
+  end
+end
