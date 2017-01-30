3eb2280bb1a5c787186577e2eb67670c060b73b6
Masayuki Higashino <msyk@hgsn.info>
2017-01-30 16:52:14 +0900

    update migrations

--- db/migrate/20170101000000_create_people.rb
+++ db/migrate/20170101000000_create_people.rb
@@ -2,7 +2,7 @@ class CreatePeople < ActiveRecord::Migration[5.0]
   def change
     create_table :people do |t|
       # Database authenticatable
-      t.string :email  # NOTE An e-mail address is not unique identifier in this application policy.
+      t.string :email  # NOTE An e-mail address is not unique identifier in this application's policy.
       t.string :encrypted_password
       # Rememberable
       t.datetime :remember_created_at
@@ -17,17 +17,16 @@ class CreatePeople < ActiveRecord::Migration[5.0]
       # UID
       t.string :uid, null:false
       # Options
-      t.string :yp_ldap_uid # dn
+      t.string :yp_ldap_uid
       t.string :yp_garoon_uid
       t.string :name
       t.string :name_ruby
       t.string :name_en
-      t.text :note
+      t.text :description
       t.timestamps
     end
-    add_index :people, :email
     add_index :people, :uid, unique:true
-    %i(yp_ldap_uid yp_garoon_uid name name_ruby name_en note).each do |column|
+    %i(email yp_ldap_uid yp_garoon_uid name name_ruby name_en description).each do |column|
       add_index :people, column
     end
   end
