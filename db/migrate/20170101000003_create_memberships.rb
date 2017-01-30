0b2c46673332f4a6199f6aec516b4486859bedfa
Masayuki Higashino <msyk@hgsn.info>
2017-01-30 16:53:13 +0900

    add a index for n x n

--- db/migrate/20170101000003_create_memberships.rb
+++ db/migrate/20170101000003_create_memberships.rb
@@ -1,10 +1,11 @@
 class CreateMemberships < ActiveRecord::Migration[5.0]
   def change
-    create_table :memberships do |t|
+    create_table :memberships, id:false do |t|
       t.references :person, foreign_key:true
       t.references :group, foreign_key:true
       t.references :title, foreign_key:true
       t.timestamps
     end
+    add_index :memberships, [:person_id, :group_id, :title_id], unique:true
   end
 end
